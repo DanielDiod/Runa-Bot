52ee64926a3cb5289fa0e508bedaa78a532f9e01
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-07-25 01:20:50 -0500

    Update _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -18,7 +18,7 @@ let img = await (await fetch(`https://tinyurl.com/2c5hk765`)).buffer()
 					description: null,
 					previewType: "PHOTO",
 					thumbnailUrl: 'https://telegra.ph/file/474de03b8c0cf87231d8e.jpg',
-		           sourceUrl: canal,
+		           sourceUrl: channel,
 		           mediaType: 1,
                    renderLargerThumbnail: false
 	    },
@@ -38,7 +38,7 @@ let img = await (await fetch(`https://tinyurl.com/2c5hk765`)).buffer()
 					previewType: "PHOTO",
 					thumbnailUrl: img,
                     thumbnail: img,
-		           sourceUrl: canal,
+		           sourceUrl: channel,
 		           mediaType: 1,
                    renderLargerThumbnail: true
 				}
