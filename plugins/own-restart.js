5b3bb020d3edff5ac5a14ce67c6663520080678c
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-25 08:07:17 -0500

    Actualizar own-restart.js

--- plugins/own-restart.js
+++ plugins/own-restart.js
@@ -4,7 +4,7 @@ var handler = async (m, { conn, isROwner, text }) => {
 
 if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
 if (conn.user.jid == conn.user.jid) {
-await conn.reply(m.chat, '☁️ *Reiniciando*', m, fake, )
+await conn.reply(m.chat, '☁️ *Reiniciando*', m, rcanal, )
 process.send('reset')
 } else throw 'eh'
 
