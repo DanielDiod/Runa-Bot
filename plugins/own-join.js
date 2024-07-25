1e6b2585ac402dbd6da34f4da41f59f96940c2be
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-07-25 13:28:50 -0500

    Update own-join.js

--- plugins/own-join.js
+++ plugins/own-join.js
@@ -5,12 +5,12 @@ var handler = async (m, { conn, text, isMods, isOwner }) => {
 let users = '500'
 let link = (m.quoted ? m.quoted.text ? m.quoted.text : text : text) || text
 let [_, code] = link.match(linkRegex) || []
-if (!code) return conn.reply(m.chat, `🎌 *Ingrese el enlace de un grupo*\n\nEjemplo, !unete`, m, fake, )
+if (!code) return conn.reply(m.chat, `🍟 *Ingrese el enlace de un grupo*\n\nEjemplo, !unete`, m, rcanal, )
 
 let gpData = await conn.groupGetInviteInfo(code).catch(e => {})
   
 if ( isMods || isOwner || m.fromMe) {
-conn.reply(m.chat, `✅ ${cb} *Se unió al grupo*`, m, fake, )
+conn.reply(m.chat, `✅ ${cb} *Se unió al grupo*`, m, rcanal, )
 await delay(5 * 5000)
 let res = await conn.groupAcceptInvite(code)
 } else {
@@ -19,7 +19,7 @@ const data = global.owner.filter(([number, _, isDeveloper]) => isDeveloper && nu
 await delay(1 * 1000)
 for (let jid of data.map(([id]) => [id] + '@s.whatsapp.net').filter(v => v != conn.user.jid)) m.reply(m.chat, `🚩 *Solicitud*\n\n*Usuario*\n` + ' wa.me/' + m.sender.split('@')[0] + '\n\n*Enlace*\n ' + link, jid)
 
-conn.reply(m.chat, `*✅ Su enlace se envió a Mí Propietario(a)*`, m, fake, )
+conn.reply(m.chat, `*✅ Su enlace se envió a Mí Propietario(a)*`, m, rcanam, )
 }
 
 }
