11b7c5f14ee46f37c1ab45913dd3f7f2624e049a
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-07-30 19:45:24 -0500

    Update info-contacto.js

--- plugins/info-contacto.js
+++ plugins/info-contacto.js
@@ -11,8 +11,8 @@ let bio = biografia.status?.toString() || 'Sin Biografía'
 let biobot = biografiaBot.status?.toString() || 'Sin Biografía'
 
 await conn.sendContactArray(m.chat, [
-[devnum, `${await conn.getName('5214434703586'+'@s.whatsapp.net')}`, `🍭 Creador`, dev, email, `🇲🇽 México`, `https://www.youtube.com/@Azami_YT`, bio],
-[`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🍧 CuriosityBot-MD`, `📵 No Hacer Spam`, email, `🇲🇽 México`, `https://github.com/AzamiJs/CuriosityBot-MD`, biobot]
+[devnum, `${await conn.getName('51955918117'+'@s.whatsapp.net')}`, `🍭 Creador`, dev, email, `Peru`, `https://github.com/DanielDiod`, bio],
+[`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🍧 Runa Bot`, `📵 No Hacer Spam`, email, `🇲🇽 México`, `https://github.com/DanielDiod/Runa-Bot`, biobot]
 ], m)
   
 }
