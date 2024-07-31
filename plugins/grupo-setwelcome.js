2048ee6b3f836573921f6702b4d1f3a0ed4a4968
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-07-31 14:49:31 -0500

    Update grupo-setwelcome.js

--- plugins/grupo-setwelcome.js
+++ plugins/grupo-setwelcome.js
@@ -5,7 +5,7 @@ if (isROwner) global.conn.welcome = text
 else if (isOwner) conn.welcome = text
 else global.db.data.chats.sWelcome = text
 
-conn.reply(m.chat, '✅ *La bienvenida se a configurado*', m)
+conn.reply(m.chat, '🧃 La bienvenida se a configurado', m)
 } else return conn.reply(m.chat, '🧃 Ingrese el texto que quiere que envie al unirse una persona', m)
 }
 handler.help = ['setwelcome']
