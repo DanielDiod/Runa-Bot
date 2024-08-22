5d4921022a2e0664ada74e088e5ebd12571a0769
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-08-22 01:07:43 -0500

    Update bot-bot.js

--- plugins/bot-bot.js
+++ plugins/bot-bot.js
@@ -6,7 +6,7 @@ let chat = global.db.data.chats[m.chat]
 if (/^bot$/i.test(m.text) && !chat.isBanned) {
 
 conn.sendPresenceUpdate('composing', m.chat)    
-conn.reply(m.chat, '🎌 *Estoy aquí para ayudarte*', m, fake, )}
+conn.reply(m.chat, '☁️ Estoy aquí para ayudarte', m)}
 
 return !0
 
