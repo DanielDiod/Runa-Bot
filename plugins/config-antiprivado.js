ac0d8348d2e45fe3fe05a01a1bcc198758d6c3ac
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-08-22 01:24:23 -0500

    Update config-antiprivado.js

--- plugins/config-antiprivado.js
+++ plugins/config-antiprivado.js
@@ -11,7 +11,7 @@ let user = global.db.data.users[m.sender]
 let bot = global.db.data.settings[this.user.jid] || {}
 
 if (bot.antiPrivate && !isOwner && !isROwner) {
-await m.reply(`🎌 *Hola* @${who.replace(/@.+/, '')}, *no puede usar este bot en chat privado*\n\nUnete al Grupo oficial para poder usar el bot\n${nn}`, false, { mentions: [who] })
+await m.reply(`☁️ Hola @${who.replace(/@.+/, '')}, no puede usar este bot en chat privado\n\nUnete al Grupo oficial para poder usar el bot\n${nn}`, false, { mentions: [who] })
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'banchat')
 
 return !1
