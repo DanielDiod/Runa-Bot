bd0ba00d72eb40ca42b24fb8e9639d58e13b7bb0
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-08-22 00:02:17 -0500

    Update bot-menu.js

--- plugins/bot-menu.js
+++ plugins/bot-menu.js
@@ -80,8 +80,7 @@ await conn.sendMessage(m.chat, {
   
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
-let menu = `⌜ *${wm}* ⌟  
-
+let menu = `  
 *•/• Información - Bot •/•*
  
 👤 Creador : Daniel 🇦🇱
