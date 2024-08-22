fb02d9381679c9a8e4cbfd894278373b63d75bc2
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-08-22 01:18:12 -0500

    Update cmd-list.js

--- plugins/cmd-list.js
+++ plugins/cmd-list.js
@@ -1,6 +1,6 @@
 var handler = async (m, {conn}) => {
 
-conn.reply(m.chat, `🎌 *LISTA - CMD*
+conn.reply(m.chat, `🍟 *LISTA - CMD*
 
 ${Object.entries(global.db.data.sticker).map(([key, value], index) => `${index + 1}.-\n*Codigo:* ${value.locked ? `_bloqueado_ ${key}` : key}\n*Comando* ${value.text}`).join('\n\n')}
 `.trim(), null, {mentions: Object.values(global.db.data.sticker).map((x) => x.mentionedJid).reduce((a, b) => [...a, ...b], [])})
