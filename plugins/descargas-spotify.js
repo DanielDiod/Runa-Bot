cb425ca835c0121b2dbf7a35d9c84aa7611ab726
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-08-23 07:24:48 -0500

    Update descargas-spotify.js

--- plugins/descargas-spotify.js
+++ plugins/descargas-spotify.js
@@ -80,14 +80,14 @@ if (!songInfo.length) throw `*No se encontró una canción.*`
 let res = songInfo[0]
 let fileSizeInMB = (await getBuffer(res.url)).length / (1024 * 1024)
 let shortURL = await getTinyURL(res.url)
-let info = `> S P O T I F Y  乂  D E S C A R G A S\n  
+let info = `- S P O T I T Y - D L \n  
 ✩ *Nombre:* ${res.name}\n
 ✩ *Artista:* ${res.artista.join(', ')}\n
 ✩ *Album:* ${res.album}\n
 ✩ *Duracion:* ${res.duracion}\n
 ✩ *Fuente:* *Spotify*\n
-✩ *Enlace:* ${shortURL}\n\n`
-'`- ↻enviando audio espere un momento soy lenta..`'
+✩ *Enlace:* ${shortURL}\n\n
+*- ↻enviando audio espere un momento soy lenta..*`
 let resImg = await fetch(res.imagen)
 let thumbb = await resImg.buffer()
 let { videos } = await search(res.name)
