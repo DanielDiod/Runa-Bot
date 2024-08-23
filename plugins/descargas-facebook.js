f7fb29cda1a7b372038d7d0099503b71cfa1e7e2
Charles Zam's <141449338+AzamiJs@users.noreply.github.com>
2024-08-22 23:21:04 -0600

    Update descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -1,25 +1,27 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) return m.reply('☁️ Ingresa un enlace de facebook');
 
-  try {
-await m.react('🕓'); 
-    const apiResponse = await fetch(`https://apis-starlights-team.koyeb.app/starlight/facebook?url=${args[0]}`);
-    const responseData = await apiResponse.json();
-    
-    if (responseData.success) {
-      const { title, links } = responseData;
-      let txt = '    `Facebook downloader`\n\n';
-      txt += `> *Titulo* : _${title}_\n`;
+if (!args[0]) {
+return m.reply('Ingrese un enlace de un *reel* de *Facebook*\n\n`Ejemplo`: .fb https://www.facebook.com/share/r/GB9op9yMyNUmFVH2/?mibextid=V2iOCx')
+}
 
-      await conn.sendMessage(m.chat, { video: { url: links[0].link }, caption: txt }, { quoted: m });
-await m.react('✅'); 
-    } else {
-    }
-  } catch {
-await m.react('❌'); 
-  }
+if (!args[0].includes('facebook')) {
+return m.reply('Enlace no válido. Compruebe el enlace')
+}
+
+try {
+const data = await fetch(`https://lolhuman.xyz/api/facebook?apikey=GataDiosV2&url=${args[0]}`)
+let video = await data.json()
+let buff = await video.result[0]
+
+const caption = `Video de Facebook`
+
+await conn.sendMessage(m.chat, { video: { url: buff }, mimetype: 'video/mp4', fileName: `video.mp4`, caption: caption, mentions: [m.sender], }, { quoted: m })
+
+} catch (e) {
+m.reply('Ha ocurrido un error al descargar su video: ' + e)
+}
 }
 
 handler.help = ['facebook *<link>*'];
