b5efc6250c8b57a2930247c355466451046aa260
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-08-23 09:04:51 -0500

    Update start-menu.js

--- plugins/start-menu.js
+++ plugins/start-menu.js
@@ -167,7 +167,7 @@ rows: [{ header: "Menu Completo", title: "", id: `.allmenu`, description: `Para 
 { header: "3049 Imperio", title: "", id: `.imperio`, description: `Saber la historia de 3049 🏫\n` },
 { header: "creador", title: "", id: `.creador`, description: `comunicate con mi creador ⚙️` }
 ]})
-await conn.sendList(m.chat, '\`•/• Menu - Lista •/•`\n\n*Creador:* Daniel\n*Versión:* 1.0.0\n\nEl Bot aun esta en desarrollo tener paciencia 🍫', null, `Clik`, listSections, { mentions: [m.sender]}, {quoted: m})
+await conn.sendList(m.chat, `\*•/• Menu - Lista •/•*\n\n${saludo} ${nombre}\n\n🚩Informaciòn:\n\n*Autor:*Daniel\n*Version:*${vs}\n\n🍟Aviso:\nEste proyecto aun sigue en mejoras espero sea de vuestro agrado`, null, `Clik`, listSections, { mentions: [m.sender]}, {quoted: m})
   } catch (e) {
     conn.reply(m.chat, '🍟 Lo sentimos, el menú tiene un error.', m)
     throw e
